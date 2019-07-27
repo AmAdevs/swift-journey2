62ea5a609fee6f0007073f0b36cd079d43cbe8df
AmAdev <aaananchai@gmail.com>
2019-07-27 16:55:25 +0700

    add AdaptiiveUIDemo

---
+++ AdaptiveUIDemo/AdaptiveUIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveUIDemo
+//
+//  Created by Ananchai Mankhong on 27/7/2562 BE.
+//  Copyright © 2562 Ananchai Mankhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
