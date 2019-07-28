ca4f8d50b96e8974be068819f307cd866ee4cea0
AmAdev <aaananchai@gmail.com>
2019-07-28 23:08:34 +0700

    KivaLoan

---
+++ KivaLoan/KivaLoan/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Ananchai Mankhong on 28/7/2562 BE.
+//  Copyright © 2562 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+struct Loan {
+    
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+}
+
